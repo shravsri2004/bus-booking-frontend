268f5d4a29364f93ff54c804207198e81f0edc2d
shravsri2004 <shrav.sri298@gmail.com>
2026-04-16 15:06:27 +0530

    added bus_route

---
+++ src/main/java/com/busbooking/entity/Bus_route.java
@@ -0,0 +1,60 @@
+package com.busbooking.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="bus_route")
+public class Bus_route {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "route_id")
+	private Integer routeId;
+	
+	@Column(name = "source")
+	private String src;
+	
+	@Column(name = "destination")
+	private String dest;
+
+	public Integer getRouteId() {
+		return routeId;
+	}
+
+	public void setRouteId(Integer routeId) {
+		this.routeId = routeId;
+	}
+
+	public String getSrc() {
+		return src;
+	}
+
+	public void setSrc(String src) {
+		this.src = src;
+	}
+
+	public String getDest() {
+		return dest;
+	}
+
+	public void setDest(String dest) {
+		this.dest = dest;
+	}
+
+	@Override
+	public String toString() {
+		return "Bus_route [routeId=" + routeId + ", src=" + src + ", dest=" + dest + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+}
