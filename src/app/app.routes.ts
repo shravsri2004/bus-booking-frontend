6981edc480db17f6da1a84d7ac54896d3f3d2081
Aashi Chauhan <aashichauhan6020@gmail.com>
2026-04-18 14:45:05 +0530

    successful

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,11 +6,10 @@ import { ScheduleComponent } from './schedule/schedule.component';
 import { SeatbookingComponent } from './seatbooking/seatbooking.component';
 
 export const routes: Routes = [
-    {path:'signup',component:SignupComponent},
-    {path:'login',component:LoginComponent},
-    {path:'booking',component:BookpageComponent},
-    {path:'schedules',component:ScheduleComponent},
-    {path:'seatbooking',component:SeatbookingComponent},
-
-    {path:'',redirectTo:'login',pathMatch:'full'}
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'booking', component: BookpageComponent },
+  { path: 'schedules', component: ScheduleComponent },
+  { path: 'seatbooking', component: SeatbookingComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
